55cac9a91a466b77ff32e836f6f72acc4cd94d97
Loushik <82611455+LoushikLK@users.noreply.github.com>
2023-08-13 15:29:48 +0530

    Update AboutSection.tsx

--- src/components/section/AboutSection.tsx
+++ src/components/section/AboutSection.tsx
@@ -18,9 +18,9 @@ const AboutSection = () => {
 
 Over time, I{"'"}ve had the awesome opportunity to dive into various projects and fine-tune my skills. These days, I{"'"}m all about crafting digital experiences that are open to everyone and cater to a wide range of client needs. From the nitty-gritty of HTML5, CSS3, and JavaScript to the magic of ReactJS, NodeJS, MongoDB, and even React Native for mobile app development – I{"'"}ve been busy exploring and mastering these technologies.
 
-But wait, there{"'"}s more! I don't just stop at creating websites that work smoothly. I{"'"}m also deeply passionate about making sure they{"'"}re easy for everyone to use, regardless of their abilities. Whether it{"'"}s tweaking a bit of code or architecting an entire REST API, I love the challenge of making things work seamlessly.
+But wait, there{"'"}s more! I don{"'"}t just stop at creating websites that work smoothly. I{"'"}m also deeply passionate about making sure they{"'"}re easy for everyone to use, regardless of their abilities. Whether it{"'"}s tweaking a bit of code or architecting an entire REST API, I love the challenge of making things work seamlessly.
 
-Speaking of challenges, I{"'"}m a firm believer in seeking out new ones. That{"'"}s why I'm always on the lookout for fresh opportunities to stretch my skills and knowledge. My portfolio showcases some of the awesome projects.
+Speaking of challenges, I{"'"}m a firm believer in seeking out new ones. That{"'"}s why I{"'"}m always on the lookout for fresh opportunities to stretch my skills and knowledge. My portfolio showcases some of the awesome projects i have done watch them below.
             </h3>
 
         
